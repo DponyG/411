9b222fc388dbf253a68fe5c7cc1b3426d7febab4
Samuel Grenon <dponyg@gmail.com>
2018-09-20 12:10:49 -0800

    Compiles Fails All Tests

---
+++ A2/build.hpp
@@ -0,0 +1,17 @@
+#ifndef BUILD_HPP
+#define BUILD_HPP
+
+#include<vector>
+
+using Bridge = std::vector<int>;
+
+
+int build(int, int, const std::vector<Bridge> &);
+using Bridge = std::vector<int>;
+
+
+
+
+
+#endif
+
