63a00b28408f0ff1b5607a6e0a8efb5791354265
Samuel Grenon <dponyg@gmail.com>
2018-09-20 13:50:44 -0800

    Begin Cycling through Vector. Passing Begining Test

--- A2/build.hpp
+++ A2/build.hpp
@@ -7,6 +7,7 @@ using Bridge = std::vector<int>;
 
 
 int build(int, int, const std::vector<Bridge> &);
+bool IsNotValid(int TestWest, int TestEast, int CurrWest, int CurrEast);
 using Bridge = std::vector<int>;
 
 
