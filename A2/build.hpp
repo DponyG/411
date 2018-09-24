8a43e0426fa7fcb9f01cd8d18a031d7a84d12adc
Samuel Grenon <dponyg@gmail.com>
2018-09-23 17:43:28 -0800

    Pushing Working Version with Updated Coding Standards

--- A2/build.hpp
+++ A2/build.hpp
@@ -7,15 +7,9 @@ using Bridge = std::vector<int>;
 
 
 int build(int, int, const std::vector<Bridge> &);
-
-
-int permutations(std::vector<Bridge> stack, const std::vector<Bridge> & bridges, int offset);
-
-bool isValid(int TestWest, int TestEast, int CurrWest, int CurrEast);
-
+bool BIsValid(const int, const int, const int, const int);
+bool BCheckStack(const std::vector<Bridge> &, const Bridge B);
 using Bridge = std::vector<int>;
-
-
 
 
 
