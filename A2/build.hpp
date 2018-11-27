93f13ba424612fd2f855982da978898679a6df8f
Samuel Grenon <dponyg@gmail.com>
2018-11-26 16:22:09 -0900

    nothing works

--- A2/build.hpp
+++ A2/build.hpp
@@ -1,19 +1,39 @@
+
+
+//CS411
+//Assignment2
+//build.hpp
+//This program generates the permutations of a vector and computes
+//the maximum total of each..
+
 #ifndef BUILD_HPP
 #define BUILD_HPP
 
 #include<vector>
 
 using Bridge = std::vector<int>;
-
+class DyBridge {
+public:
+	DyBridge(const std::vector<Bridge> & Bridge, int West, int East);
+	bool BridgePlaceable(int, int);
+	int Recurse(int index);
+	int GetAnswer();
+private:
+	std::vector<std::vector<int>>BridgeVec;
+	std::vector<int> WorkVec;
+	int Answer = 0;
+	int WestCities;
+	int EastCities;
+};
 
 int build(int, int, const std::vector<Bridge> &);
-bool BIsValid(const int, const int, const int, const int);
-bool BCheckStack(const std::vector<Bridge> &, const Bridge B);
-using Bridge = std::vector<int>;
+bool BSortVec(std::vector<int> a, std::vector<int> b);
+
 
 
 
 
 
 #endif
+
 
